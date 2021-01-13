66c9995677e50bbbdc1d1610ef4089e453eb6b22
hui <learner.hui@gmail.com>
2021-01-13 16:46:53 +0800

    TLS 证书认证

--- server/simple_server/server.go
+++ server/simple_server/server.go
@@ -3,9 +3,11 @@ package main
 import (
     "context"
     "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials"
     "log"
     pb "mayGo/proto"
     "net"
+    "os"
 )
 
 type SearchService struct{}
@@ -17,7 +19,13 @@ func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.Se
 const PORT = "9001"
 
 func main() {
-    server := grpc.NewServer()
+    dir, _ := os.Getwd()
+    c, err := credentials.NewServerTLSFromFile(dir + "/conf/server.pem", dir + "/conf/server.key")
+    if err != nil {
+        log.Fatalf("credentials.NewServerTLSFromFile err: %v", err)
+    }
+    
+    server := grpc.NewServer(grpc.Creds(c))
     pb.RegisterSearchServiceServer(server, &SearchService{})
     
     lis, err := net.Listen("tcp", ":"+PORT)
